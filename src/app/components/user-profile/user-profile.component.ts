121564a207420d6b9b45fc66a827b7ce15b855cd
darshithv <darshithv3392@gmail.com>
2024-03-31 11:30:48 +0530

    interpolation, property binding

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.scss'
 })
+  
 export class UserProfileComponent {
-
+  name = "darshith"
+  status = "single"
+  salary = 40000
+  isBtnDisabled = false
+  
+  onChange(e:Event) {
+    const value = (e.target as HTMLInputElement).value
+    console.log(value)
+  }
 }
