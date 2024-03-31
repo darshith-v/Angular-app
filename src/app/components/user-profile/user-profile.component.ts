42caaa954aea547c4a97cd394bab90c985bb4a88
darshithv <darshithv3392@gmail.com>
2024-03-31 13:56:18 +0530

    control flow (@if, @for)

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -10,15 +10,21 @@ import { FormsModule } from '@angular/forms';
 })
   
 export class UserProfileComponent {
-  name = "darshith"
-  status = "single"
-  salary = 40000
-  isBtnDisabled = false
+  // name = "darshith"
+  // status = "single"
+  // salary = 40000
+  // isBtnDisabled = false
 
-  inputVal = ''
+  // inputVal = ''
   
-  onChange(e:Event) {
-    const value = (e.target as HTMLInputElement).value
-    this.inputVal = value
-  }
+  // onChange(e:Event) {
+  //   const value = (e.target as HTMLInputElement).value
+  //   this.inputVal = value
+  // }
+
+  users = [
+    { name: "Darshith", isSingle: true, salary: 100000 },
+    { name: "Ravikiran", isSingle: false, salary: 600000 },
+    {name: "Sharath", isSingle: true, salary:800000}
+  ]
 }
